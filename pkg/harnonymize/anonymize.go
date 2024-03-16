1731f5b908d7e84c1c3b77f6249409fc2d847b7a
jfallis <mrjohnfallis+jfallis@gmail.com>
2024-03-16 15:13:11 +0000

    simplify code

--- pkg/harnonymize/anonymize.go
+++ pkg/harnonymize/anonymize.go
@@ -10,11 +10,8 @@ import (
 )
 
 const (
-	cookieName = "cookie"
-	authName   = "auth"
-)
-
-const (
+	cookieName   = "cookie"
+	authName     = "auth"
 	redactedText = "** HARnonymize removed content **"
 )
 
@@ -38,43 +35,40 @@ func (c *Config) Read(file HAR) error {
 		return ErrNotHARFile
 	}
 
-	name := fmt.Sprintf("%s/%s", file.Path, file.Name)
-	context, readErr := os.ReadFile(name)
-	if readErr != nil {
-		return readErr
+	content, err := os.ReadFile(fmt.Sprintf("%s/%s", file.Path, file.Name))
+	if err != nil {
+		return err
 	}
 
-	return file.HAR.UnmarshalJSON(context)
+	return file.HAR.UnmarshalJSON(content)
 }
 
 func (c *Config) Write(file HAR) error {
-	name := fmt.Sprintf("%s/anonymized_%s", file.Path, file.Name)
-
 	data, err := file.HAR.MarshalJSON()
 	if err != nil {
 		return err
 	}
 
-	return os.WriteFile(name, data, fs.ModePerm)
+	return os.WriteFile(fmt.Sprintf("%s/anonymized_%s", file.Path, file.Name), data, fs.ModePerm)
 }
 
 func (c *Config) Anonymize(file HAR) {
-	for x := range file.HAR.Log.Entries {
-		file.HAR.Log.Entries[x].Request.Cookies = nil
-		file.HAR.Log.Entries[x].Response.Cookies = nil
-		redactHeaders(file.HAR.Log.Entries[x].Request.Headers)
-		redactHeaders(file.HAR.Log.Entries[x].Response.Headers)
-		c.redactByContent(file.HAR.Log.Entries[x].Response.Content)
+	for i := range file.HAR.Log.Entries {
+		entry := file.HAR.Log.Entries[i]
+
+		entry.Request.Cookies = nil
+		entry.Response.Cookies = nil
+
+		redactHeaders(entry.Request.Headers)
+		redactHeaders(entry.Response.Headers)
+
+		c.redactContent(entry.Response.Content)
 	}
 }
 
-func (c *Config) redactByContent(content *har.Content) {
-	for _, keywords := range c.BlockContentKeywords {
-		if stringContains(content.Text, keywords) {
-			if strings.EqualFold(content.MimeType, "application/json") {
-				content.Text = fmt.Sprintf(`{text: %q}`, redactedText)
-				break
-			}
+func (c *Config) redactContent(content *har.Content) {
+	for _, keyword := range c.BlockContentKeywords {
+		if strings.Contains(strings.ToLower(content.Text), keyword) {
 			content.Text = redactedText
 			break
 		}
@@ -82,16 +76,10 @@ func (c *Config) redactByContent(content *har.Content) {
 }
 
 func redactHeaders(headers []*har.NameValuePair) {
-	for y := range headers {
-		if stringContains(headers[y].Name, authName) {
-			headers[y].Value = redactedText
-		}
-		if stringContains(headers[y].Name, cookieName) {
-			headers[y].Value = redactedText
+	for i := range headers {
+		header := headers[i]
+		if strings.Contains(strings.ToLower(header.Name), authName) || strings.Contains(strings.ToLower(header.Name), cookieName) {
+			header.Value = redactedText
 		}
 	}
-}
-
-func stringContains(str, substr string) bool {
-	return strings.Contains(strings.ToLower(str), substr)
 }
